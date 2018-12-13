3d869577750d4b8e3d2ed56380127c383fa20996
audhalbritter <audhalbritter.com>
2018-12-13 18:31:04 +0100

    fix data repo
    fix cleaning code

--- R/ImportCleanAndMakeList_CN_Gongga.R
+++ R/ImportCleanAndMakeList_CN_Gongga.R
@@ -8,7 +8,7 @@ ImportCommunity_CN_Gongga <- function(){
   sapply(fl, source)
   
   ## ---- load_community
-  con <- src_sqlite(path = "data/transplant.sqlite", create = FALSE)
+  con <- src_sqlite(path = "data/CN_Gongga/transplant.sqlite", create = FALSE)
   # need to move all code to dplyr for consistancy
   
   #load cover data and metadata
@@ -24,7 +24,7 @@ ImportTaxa_CN_Gongga <- function(){
   sapply(fl, source)
   
   ## ---- load_community
-  con <- src_sqlite(path = "data/transplant.sqlite", create = FALSE)
+  con <- src_sqlite(path = "data/CN_Gongga/transplant.sqlite", create = FALSE)
   
   # need to move all code to dplyr for consistancy
   taxa_CN_Gongga <- tbl(con, "taxon") %>%
@@ -55,25 +55,26 @@ CleanTrait_CN_Gongga <- function(dat){
 }
 
 # Cleaning community data
-CleanCommunity_CN_Gongga <- function(dat){
-  dat2 <- dat %>% 
+CleanCommunity_CN_Gongga <- function(community_CN_Gongga_raw){
+  dat2 <- community_CN_Gongga_raw %>% 
     filter(TTtreat != c("OTC")) %>% 
-    rename(Year = year, Treatment = TTtreat, Taxon = speciesName, Cover = cover) %>% 
+    rename(Year = year, Treatment = TTtreat, Cover = cover, SpeciesName = speciesName) %>% 
     mutate(Gradient = "CN_Gongga",
-                      Country = as.character("China"),) %>% 
-    mutate(Taxon = recode(Taxon, "Potentilla stenophylla var. emergens" = "Potentilla stenophylla")) %>% 
+                      Country = as.character("China"),
+           Treatment = recode(Treatment, "control" = "Control", "local" = "LocalControl", "warm1" = "Warm", "cool1" = "Cold", "warm3" = "WarmLong", "cool3" = "ColdLong")) %>% 
+    mutate(SpeciesName = recode(SpeciesName, "Potentilla stenophylla var. emergens" = "Potentilla stenophylla")) %>% 
     filter(!is.na(Cover), !Cover == 0)
   
   return(dat2)
 }
 
+  
 # Cleaning China meta community data
-CleanMetaCommunity_CN_Gongga <- function(dat){
-  dat2 <- dat %>% 
+CleanMetaCommunity_CN_Gongga <- function(metaCommunity_CN_Gongga_raw){
+  dat2 <- metaCommunity_CN_Gongga_raw %>% 
     select(PlotID, Year, Moss, Lichen2, Litter, BareGround, Rock, Vascular, Bryophyte, Lichen, MedianHeight_cm, MedianMossHeight_cm) %>% 
     mutate(Gradient = "CN_Gongga",
-           Country = as.character("China"),
-           Site = substr(PlotID, 1,1))
+           Country = as.character("China"))
   return(dat2)
 }
 
@@ -95,17 +96,18 @@ CleanMeta_CN_Gongga <- function(dat){
 ImportClean_CN_Gongga <- function(){
   
   ### IMPORT DATA
-  meta_CN_Gongga_raw = get(load(file = file_in("data/metaCN_Gongga.Rdata")))
-  metaCommunity_CN_Gongga_raw = get(load(file = file_in("data/metaCommunityCN_Gongga_2012_2016.Rdata")))
+  meta_CN_Gongga_raw = get(load(file = file_in("data/CN_Gongga/metaCN_Gongga.Rdata")))
+  metaCommunity_CN_Gongga_raw = get(load(file = file_in("data/CN_Gongga/metaCommunityCN_Gongga_2012_2016.Rdata")))
   community_CN_Gongga_raw = ImportCommunity_CN_Gongga()
   taxa_CN_Gongga = ImportTaxa_CN_Gongga()
-  trait_CN_Gongga_raw = get(load(file = file_in("data/traits_2015_2016_China.Rdata")))
+  trait_CN_Gongga_raw = get(load(file = file_in("data/CN_Gongga/traits_2015_2016_China.Rdata")))
 
   ### CLEAN DATA SETS
   ## CN_Gongga
   meta_CN_Gongga = CleanMeta_CN_Gongga(meta_CN_Gongga_raw)
   metaCommunity_CN_Gongga = CleanMetaCommunity_CN_Gongga(metaCommunity_CN_Gongga_raw)
   community_CN_Gongga = CleanCommunity_CN_Gongga(community_CN_Gongga_raw)
+  taxa_CN_Gongga = ImportTaxa_CN_Gongga()
   trait_CN_Gongga = CleanTrait_CN_Gongga(trait_CN_Gongga_raw)
   
   # Make list
