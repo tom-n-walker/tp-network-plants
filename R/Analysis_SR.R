8f7be2e7704d342b2d41156b0fbdeae58fce42cc
“chelseachisholm” <chelsea.chisholm@gmail.com>
2018-12-14 14:45:03 +0100

    Fixed issues with analysis function

--- R/Analysis_SR.R
+++ R/Analysis_SR.R
@@ -4,13 +4,13 @@
 #summarize data and analyze Species richness ~treatment by Year and Site
 AnalyzeSR <- function(x) {
 
-lm_Gongga<-x$community %>% group_by(Year, destSiteID, Treatment, destPlotID) %>% 
+lm_Site<-x$community %>% group_by(Year, destSiteID, Treatment, destPlotID) %>% 
   filter(Cover>0, Treatment %in% c('Warm', 'Control', 'LocalControl', destSiteID == 'L')) %>% 
   summarize(SR=n_distinct(species)) %>%
   group_by(Year) %>%
   do(lmSR = lm(SR~Treatment, data=.)) %>%
   tidy(lmSR)
 
-return(lm_Gongga) }
+return(lm_Site) }
 
            
