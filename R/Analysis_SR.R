ac0a7038263b0c50470dbbc25c8a149c4cbb2102
“chelseachisholm” <chelsea.chisholm@gmail.com>
2018-12-14 14:38:00 +0100

    Added test analysis function

---
+++ R/Analysis_SR.R
@@ -0,0 +1,16 @@
+##### Simple Test Analysis ####
+#C&D 14.12.2018
+
+#summarize data and analyze Species richness ~treatment by Year and Site
+AnalyzeSR <- function(x) {
+
+lm_Gongga<-x$community %>% group_by(Year, destSiteID, Treatment, destPlotID) %>% 
+  filter(Cover>0, Treatment %in% c('Warm', 'Control', 'LocalControl', destSiteID == 'L')) %>% 
+  summarize(SR=n_distinct(species)) %>%
+  group_by(Year) %>%
+  do(lmSR = lm(SR~Treatment, data=.)) %>%
+  tidy(lmSR)
+
+return(lm_Gongga) }
+
+           
