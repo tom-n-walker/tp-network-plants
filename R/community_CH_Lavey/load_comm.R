65907620c40da20bb8b815d57f27012c65ecbe88
“chelseachisholm” <chelsea.chisholm@gmail.com>
2019-02-19 18:01:16 +0100

    Added community cover R script for Lavey CH.

---
+++ R/community_CH_Lavey/load_comm.R
@@ -0,0 +1,51 @@
+###### Lavey community data ####
+#Chelsea Chisholm, 19.02.2018
+
+load_comm <- function(con) {
+  require("tidyverse")
+  require("readxl")
+  
+path <- "PRA_PRAturf.xlsx"
+  
+cover_PRA <- path %>% 
+    excel_sheets() %>%
+    set_names() %>% 
+    map(read_excel, path = path) 
+
+#to do: remove Feuile sheet, merge (not bind_rows, cols not identical across sheets)
+#check that not duplicating columns (likely...)
+#now iterate this over all files, make a column for each file name
+#and then clean.
+
+
+
+
+##extra stuff from gonga/norway
+# make fat table
+cover <- cover.thin %>% spread(key = species, value = cover)
+
+#make meta data
+cover.meta <- cover %>% select(siteID:destSiteID) %>% 
+  mutate(TTtreat = factor(TTtreat, levels = c("TTC","TT1", "TT2", "TT3", "TT4")))
+
+
+turfs <- cover.meta[!duplicated(cover.meta$turfID),]
+
+cover <- cover %>% select(-(siteID:destSiteID))
+
+#clear up
+rm(siri.fix, siriLOW, siri)
+
+table(cover.meta$turfID, cover.meta$year)   
+table(cover.meta$year, cover.meta$siteID, cover.meta$TTtreat)         
+
+alltaxa <- TRUE
+propertaxa <- !names(cover) %in% c("NID.seedling", "Car.sp", "Hie.sp", "Luz.sp",  "NID.gram", "NID.herb", "NID.rosett", "Pyr.sp")
+noNIDseedlings <- !names(cover) %in% c("NID.seedling")
+
+turfs$newTT <- turfs$TTtreat  #alternative TTtreat with combined controls
+levels(turfs$newTT)[1:2] <- "control"
+
+# save(cover, cover.thin, cover.meta, turfs, file = "cover.Rdata")
+return(cover.thin)
+}
