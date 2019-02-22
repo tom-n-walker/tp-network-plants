21f998536aa05d2f32e17e02069abb63f54e51b7
“chelseachisholm” <chelsea.chisholm@gmail.com>
2019-02-22 17:19:28 +0000

    Collating lavey data into a single dataframe

--- R/community_CH_Lavey/load_comm.R
+++ R/community_CH_Lavey/load_comm.R
@@ -1,69 +1,68 @@
 ###### Lavey community data ####
 #Chelsea Chisholm, 19.02.2018
 
-load_comm <- function(cover) {
+setwd("./data/CH_Lavey/2017/cover")
+#load_comm <- function(cover) {
   require("tidyverse")
-  require("readxl")
   
-path <- "PRA_PRAturf.xlsx"
-cover_PRA <- lapply(excel_sheets(path), read_excel, path = path)
-cover_PRA <- cover_PRA[-1]
-lapply(cover_PRA, sumcov)
-sumcov(cover_PRA)
-sumcov <- function(x) {
-  x %>% select(-1) %>% 
-    colSums() 
-}
-cover_PRA %>% 
-cover_PRA <- path %>% 
-    excel_sheets() %>%
-    filter(!1)
-    set_names() %>% 
-    map(read_excel, path = path) 
+  
+  collector <- function (x) {
+    cover <- excel_sheets(x) %>% 
+      set_names() %>% 
+      map_df(
+        ~ read_xlsx(path = x, sheet = .x),
+        .id = "plotID") %>% 
+      group_by(plotID) %>% 
+      summarise_if(is.numeric, sum)
+    return(cover)
+  }
+ 
+  path <- list.files('.')
+  cover <- data_frame(siteID = path) %>% 
+    mutate(file_contents = map(siteID, collector)) #%>%
+    mutate(siteID = gsub(pattern = 'turf.xlsx', replacement='', filename)) %>%
+    unnest() %>% select(-filename, -Dead, -contains('FOCAL'), -`NA`) #%>%
+    
+  cover %>% arrange() 
+    
+  
 
-rbind.all.columns <- function(x, y) {
-  x.diff <- setdiff(colnames(x), colnames(y))
-  y.diff <- setdiff(colnames(y), colnames(x))
-  x[, c(as.character(y.diff))] <- NA
-  y[, c(as.character(x.diff))] <- NA
-  return(rbind(x, y))
-}
-all.data <- Reduce(rbind.all.columns, cover_PRA )
-
-#to do: remove Feuile sheet, merge (not bind_rows, cols not identical across sheets)
-#check that not duplicating columns (likely...)
-#now iterate this over all files, make a column for each file name
-#and then clean.
-
-
-
-
-##extra stuff from gonga/norway
-# make fat table
-cover <- cover.thin %>% spread(key = species, value = cover)
-
-#make meta data
-cover.meta <- cover %>% select(siteID:destSiteID) %>% 
-  mutate(TTtreat = factor(TTtreat, levels = c("TTC","TT1", "TT2", "TT3", "TT4")))
-
-
-turfs <- cover.meta[!duplicated(cover.meta$turfID),]
-
-cover <- cover %>% select(-(siteID:destSiteID))
-
-#clear up
-rm(siri.fix, siriLOW, siri)
-
-table(cover.meta$turfID, cover.meta$year)   
-table(cover.meta$year, cover.meta$siteID, cover.meta$TTtreat)         
-
-alltaxa <- TRUE
-propertaxa <- !names(cover) %in% c("NID.seedling", "Car.sp", "Hie.sp", "Luz.sp",  "NID.gram", "NID.herb", "NID.rosett", "Pyr.sp")
-noNIDseedlings <- !names(cover) %in% c("NID.seedling")
-
-turfs$newTT <- turfs$TTtreat  #alternative TTtreat with combined controls
-levels(turfs$newTT)[1:2] <- "control"
-
-# save(cover, cover.thin, cover.meta, turfs, file = "cover.Rdata")
-return(cover.thin)
-}
+  
+  
+  #to do: remove Feuile sheet, create site ID to destination ID (and I guess metadata)
+  #and then clean.
+  
+  
+  
+#   
+#   ##extra stuff from gonga/norway
+#   # make fat table
+#   cover <- cover.thin %>% spread(key = species, value = cover)
+#   
+#   #make meta data
+#   cover.meta <- cover %>% select(siteID:destSiteID) %>% 
+#     mutate(TTtreat = factor(TTtreat, levels = c("TTC","TT1", "TT2", "TT3", "TT4")))
+#   
+#   
+#   turfs <- cover.meta[!duplicated(cover.meta$turfID),]
+#   
+#   cover <- cover %>% select(-(siteID:destSiteID))
+#   
+#   #clear up
+#   rm(siri.fix, siriLOW, siri)
+#   
+#   table(cover.meta$turfID, cover.meta$year)   
+#   table(cover.meta$year, cover.meta$siteID, cover.meta$TTtreat)         
+#   
+#   alltaxa <- TRUE
+#   propertaxa <- !names(cover) %in% c("NID.seedling", "Car.sp", "Hie.sp", "Luz.sp",  "NID.gram", "NID.herb", "NID.rosett", "Pyr.sp")
+#   noNIDseedlings <- !names(cover) %in% c("NID.seedling")
+#   
+#   turfs$newTT <- turfs$TTtreat  #alternative TTtreat with combined controls
+#   levels(turfs$newTT)[1:2] <- "control"
+#   
+#   # save(cover, cover.thin, cover.meta, turfs, file = "cover.Rdata")
+#   return(cover.thin)
+# }
+# )
+#   
