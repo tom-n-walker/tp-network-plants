a091c8f541cb0ae821f0db6bdb7b60406042fb6d
cchelsea <cchelsea@poemac-106-chisholm.local>
2019-04-09 22:09:26 +0200

    Added rda, SR, overlap function to analysis script

--- R/Analysis_SR.R
+++ R/Analysis_SR.R
@@ -7,23 +7,62 @@ NMDS <- function(x) {
   x %>% metaMDS(.) 
 }
 
-library(vegan)
-library(ggord) #use pack for github repos
+library(ggfortify) #use pack for github repos
+
 AnalyzeSR <- function(p) {
 
-  rar<- x$community %>% select(destSiteID, destPlotID, Treatment, Year, SpeciesName, Cover) %>% 
-    filter(Year==max(Year)) %>%
-    spread(SpeciesName, Cover) %>% replace_na(0)
-    ggord(metaMDS(rar[,5:99]), rar$Treatment, poly = FALSE, polylntyp = rar$Treatment)
+  #GET SPECIES RICHNESS AT PLOT LEVEL PER TREATMENT*SITE
+  SR <- x$community %>% group_by(destSiteID, Treatment, turfID) %>% summarize(SR=n_distinct(SpeciesName)) %>%
+    ggplot(aes(x=Treatment, y=SR)) + geom_boxplot() + facet_wrap(~destSiteID)
+    #overlap of species between low and high sites
+ 
+  #GET OVERLAP OF HIGH AND LOW SPECIES
+  SR <- x$community %>% group_by(destSiteID, Treatment) %>% 
+     nest() %>% 
+     mutate(splist = map(data, ~select(., SpeciesName))) %>%
+     filter(!Treatment=='Warm') %>%
+     select(-data, -Treatment) %>%
+     arrange(destSiteID) %>%
+     summarise(overlap = length(intersect(splist[[1]]$SpeciesName, splist[[2]]$SpeciesName)))
 
-}
-lm_Site<-x$community %>% group_by(Year, destSiteID, Treatment, destPlotID) %>% 
-  filter(Cover>0) %>% 
-  summarize(SR=n_distinct(SpeciesName)) %>%
-  group_by(Year) %>%
-  do(lmSR = lm(SR~Treatment, data=.)) %>%
-  tidy(lmSR)
+  
+ #### Code to produce RDA per site (for final year, low site treatments)  
+   library(vegan)
+   data.list<- list(a = NO_Ulvhaugen, b=NO_Skjellingahaugen)
+   
+   rda1<- data.list %>% map_df('community', .id='Gradient') %>% 
+     select(Gradient, destSiteID, turfID, Treatment, Year, SpeciesName, Cover) %>% 
+     filter(!is.na(SpeciesName), destSiteID %in% c('Fauske','Ovstedal'), Treatment %in% c('TT2', 'Control')) %>%
+     group_by(Gradient, Treatment) %>% 
+     filter(Year==max(Year)) %>% select(-Year, -destSiteID) %>%
+     nest() %>%
+      mutate(comm = map(data, ~spread(., SpeciesName, Cover, fill=0)),
+             rda = map(comm, ~{
+               com <- select(., -(turfID:Treatment))
+               pred <- select(., turfID:Treatment)
+               rda(com ~ Treatment, pred)}),
+             rda_output = map2(.x=rda, .y=comm, ~fortify(.x, display = c("wa","cn"))))
+   
+   rda1<- data.list %>% map2(.x='community', .y='meta', ~full_join(., by='destSiteID'))
+     reduce(left_join, by=c('destSiteID'))
+     
+     data.list %>% map_df('community', .id='Gradient') %>% 
+     select(Gradient, destSiteID, turfID, Treatment, Year, SpeciesName, Cover) %>% 
+     filter(!is.na(SpeciesName), destSiteID %in% c('Fauske','Ovstedal'), Treatment %in% c('TT2', 'Control')) %>%
+     group_by(Gradient, Treatment) %>% 
+     filter(Year==max(Year)) %>% select(-Year, -destSiteID) %>%
+     nest() %>%
+     mutate(comm = map(data, ~spread(., SpeciesName, Cover, fill=0)),
+            rda = map(comm, ~{
+              com <- select(., -(turfID:Treatment))
+              pred <- select(., turfID:Treatment)
+              rda(com ~ Treatment, pred)}),
+            rda_output = map2(.x=rda, .y=comm, ~fortify(.x, display = c("wa","cn"))))
+             
+               
+            #pca_graph = m)
+                       #layers = c("sites", "centroids", "biplot")
+                      
 
-return(lm_Site) }
 
            
