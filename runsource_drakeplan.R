d2f5c3c23e2d56d9ec5e16fac992a8837a88347f
DagmarEgelkraut <dagmaregelkraut@gmail.com>
2019-11-28 11:46:12 +0100

    update

--- runsource_drakeplan.R
+++ runsource_drakeplan.R
@@ -1,1 +1,8 @@
-Source
+library("drake")
+
+r_make(source = "TransPlant_DrakePlan.R")
+
+failed()
+
+#view dependency graph
+r_vis_drake_graph(source = "TransPlant_DrakePlan.R", targets_only = TRUE)
