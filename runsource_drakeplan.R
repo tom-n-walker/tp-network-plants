236410dc0bf0e8d676800046178428de8a32d641
chelseachisholm <chelsea.chisholm@gmail.com>
2020-05-14 12:08:08 +0200

    Fixed deprecated failed() function

--- runsource_drakeplan.R
+++ runsource_drakeplan.R
@@ -10,7 +10,7 @@ library("readxl")
 r_make(source = "TransPlant_DrakePlan.R")
 loadd()
 
-failed()
+drake_failed()
 
 #view dependency graph
 r_vis_drake_graph(source = "TransPlant_DrakePlan.R", targets_only = TRUE)
