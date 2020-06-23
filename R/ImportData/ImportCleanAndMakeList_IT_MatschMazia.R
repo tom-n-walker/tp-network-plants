c32c2c5bd51b64c79bef48b8d6438a9a5e41de8f
chelseachisholm <chelsea.chisholm@gmail.com>
2020-06-23 15:15:16 +0200

    Fixed plotId and year combination issue, now all labels are correct. Some issues still with the treatment codes being off (destControl and warmed too similar from year 1). Sent email for clarification.

--- R/ImportData/ImportCleanAndMakeList_IT_MatschMazia.R
+++ R/ImportData/ImportCleanAndMakeList_IT_MatschMazia.R
@@ -15,7 +15,7 @@ ImportCommunity_IT_MatschMazia <- function(){
 # Cleaning Lautaret community data
 CleanCommunity_IT_MatschMazia <- function(community_IT_MatschMazia_raw){
   dat <- community_IT_MatschMazia_raw %>% 
-    rename(Year=year, Elevation=elevation, treat = Treatment) %>%
+    rename(Year=year, Elevation=elevation) %>%
     gather(SpeciesName, Cover, -destSiteID, -destPlotID, -Elevation, -treat, -Year) %>%
     filter(!is.na(Cover), Elevation !=1950) %>%
     mutate(SpeciesName = gsub('\\_', ' ', SpeciesName)) %>%
@@ -27,6 +27,7 @@ CleanCommunity_IT_MatschMazia <- function(community_IT_MatschMazia_raw){
     select(Year, destSiteID, originSiteID, destPlotID, Treatment, SpeciesName, Cover, -treat) %>% 
     extract(destPlotID, into = c("PlotID", "year"), "(.*)_([^_]+)$") %>% 
     mutate(destPlotID = paste(originSiteID, destSiteID, PlotID, sep='_')) %>%  
+    mutate(Year=as.numeric(year)+2000) %>%
     select(-PlotID, -year) %>% 
     mutate(UniqueID = paste(Year, destPlotID, sep='_'))  
   #  mutate(destPlotID = as.character(destPlotID), destBlockID = if (exists('destBlockID', where = .)) as.character(destBlockID) else NA)
@@ -36,7 +37,7 @@ CleanCommunity_IT_MatschMazia <- function(community_IT_MatschMazia_raw){
     group_by_at(vars(-SpeciesName, -Cover)) %>%
     summarise(SpeciesName = "Other",Cover = 100 - sum(Cover)) %>%
     bind_rows(dat) %>% 
-    filter(Cover > 0)  %>% #omg so inelegant
+    filter(Cover > 0)  %>% 
     mutate(Total_Cover = sum(Cover), Rel_Cover = Cover / Total_Cover)
   
   comm <- dat2 %>% filter(!SpeciesName %in% c('Other')) 
--- R/ImportData/community_IT_MatschMazia/loadcomm_IT.R
+++ R/ImportData/community_IT_MatschMazia/loadcomm_IT.R
@@ -4,12 +4,16 @@
 load_cover_IT_MatschMazia <- function(){
   #import data
   low <- read_excel("data/IT_MatschMazia/IT_MatschMazia_commdata/VegData10-13.csv.xlsx", sheet=1)
-  colnames(low)[3] <- 'Treatment'
+  colnames(low)[3] <- 'treat'
   high <- read_excel("data/IT_MatschMazia/IT_MatschMazia_commdata/VegData10-13.csv.xlsx", sheet=2)
-  colnames(high)[2] <- 'Treatment'
+  colnames(high)[2] <- 'treat'
   
-  low <- low %>% mutate(destSiteID='Low', destPlotID=`internal ID`) %>% select(-Bezeichnung, -`Gradient Name`, -`internal ID`) 
-  high <- high %>% mutate(destSiteID='High', destPlotID=`internal ID`) %>% select(-`Gradient Name`, -`internal ID`) 
+  low <- low %>% mutate(destSiteID='Low', destPlotID=`internal ID`) %>% 
+    mutate(year = as.numeric(str_sub(`internal ID`,-2,-1))+2000) %>%
+    select(-Bezeichnung, -`Gradient Name`, -`internal ID`) 
+  high <- high %>% mutate(destSiteID='High', destPlotID=`internal ID`) %>% 
+    mutate(year = as.numeric(str_sub(`internal ID`,-2,-1))+2000) %>%
+    select(-`Gradient Name`, -`internal ID`) 
   
   cover <- bind_rows(low, high)
   
